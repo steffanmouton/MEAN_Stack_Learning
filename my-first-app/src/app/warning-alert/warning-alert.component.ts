63746c36ea0f435399445b4a7d54b797ded432cf
Steffan Mouton <smouton8993@gmail.com>
2020-10-22 14:10:51 -0500

    Added Warning and Success Alerts.
    Assignment completed.

---
+++ my-first-app/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-warning',
+    templateUrl: './warning-alert.component.html',
+    styleUrls: ['./warning-alert.component.css']
+})
+export class WarningAlertComponent
+{
+
+}
